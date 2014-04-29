6874d95bf73039b03112cb2b11386c51897d4972
densmolov <densmolov@gmail.com>
2014-04-30 01:04:13 +0400

    First Commit

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -2,7 +2,6 @@ var discsQuantity = 0;
 var totalPages = 0;
 var pageNumber = 1;
 
-
 function submitForm() {
     $.ajax({
         type: "POST",
@@ -10,10 +9,11 @@ function submitForm() {
         data: $("#createDiscForm").serialize(),
         success: function () {
             if (formValidation()) {
-            $("#createDiscForm").each(function () {
-                this.reset();
-            });
-            toastr.success("The CD disc has been added successfully.");}
+                $("#createDiscForm").each(function () {
+                    this.reset();
+                });
+                toastr.success("The CD disc has been added successfully.");
+            }
         },
         error: function (e) {
             toastr.error("Error: " + e);
@@ -28,87 +28,92 @@ function formValidation() {
     }
 }
 
-
 function downloadCatalog() {
     window.location = '/downloadCatalog';
 }
-
 
 function redirectToDownloadFilePage() {
     $("#mainDiv").load("pages/downloadXMLfile.html #secondDiv");
 }
 
-
 function redirectToCreateDiscPage() {
     $("#mainDiv").load("pages/index.html #secondDiv");
 }
 
-
 function redirectToShowAllPage() {
-    $("#mainDiv").load("pages/showAll.html #secondDiv");
-    renderTheTable();
+    $("#mainDiv").load("pages/showAll.html #secondDiv", function (responseTxt, statusTxt, xhr) {
+        if (statusTxt == "success") {
+            ajaxCallTable();
+        }
+    });
 }
-
 
 function nextPageClick() {
     pageNumber++;
-    renderTheTable();
+    ajaxCallTable();
 }
 
 function previousPageClick() {
-    pageNumber--;
-    renderTheTable();
+    if (pageNumber > 1) {
+        pageNumber--;
+    }
+    ajaxCallTable();
 }
 
 function firstPageClick() {
     pageNumber = 1;
-    renderTheTable();
+    ajaxCallTable();
 }
 
 function lastPageClick() {
     pageNumber = totalPages;
-    renderTheTable();
+    ajaxCallTable();
 }
 
-
-function renderTheTable() {
-    $.ajax({
-        type: "GET",
-        url: "/showData" + '?' + $.param({pageNumber: this.pageNumber}),
-        async: false,
-        headers: {
-            Accept: "text/plain; charset=UTF-8",
-            "Content-Type": "text/plain; charset=UTF-8"
-        },
-        success: function (incomingString) {
-            console.log("incomingString = " + incomingString);
-            var parser = new DOMParser();
-            var xmlDoc = parser.parseFromString(incomingString, "text/xml");
-            discsQuantity = xmlDoc.getElementsByTagName("PAGES")[0].childNodes[0].nodeValue;
-            updatePaging();
-            var allCdNodes = xmlDoc.getElementsByTagName("CD");
-            var str = ("<table class=\"table table-condensed\">" +
-                "<tr><td>TITLE</td><td>ARTIST</td><td>COUNTRY</td>" +
-                "<td>COMPANY</td><td>PRICE</td><td>YEAR</td></tr>");
-            for (i = 0; i < allCdNodes.length; i++) {
-                str = str + ("<tr><td>");
-                str = str + (allCdNodes[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue);
-                str = str + ("</td><td>");
-                str = str + (allCdNodes[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue);
-                str = str + ("</td><td>");
-                str = str + (allCdNodes[i].getElementsByTagName("COUNTRY")[0].childNodes[0].nodeValue);
-                str = str + ("</td><td>");
-                str = str + (allCdNodes[i].getElementsByTagName("COMPANY")[0].childNodes[0].nodeValue);
-                str = str + ("</td><td>");
-                str = str + (allCdNodes[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue);
-                str = str + ("</td><td>");
-                str = str + (allCdNodes[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue);
-                str = str + ("</td></tr>");
-            }
-            str = str + "</table>";
-            document.getElementById("tableCD").innerHTML = str;
+function ajaxCallTable() {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {// code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            renderTheTable(xmlhttp.responseText);
         }
-    })
+    };
+    xmlhttp.open("GET", "/showData" + '?' + $.param({pageNumber: this.pageNumber}), true);
+    xmlhttp.send();
+}
+
+function renderTheTable(incomingString) {
+    console.log("incomingString = " + incomingString);
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(incomingString, "text/xml");
+    discsQuantity = xmlDoc.getElementsByTagName("PAGES")[0].childNodes[0].nodeValue;
+    updatePaging();
+    var allCdNodes = xmlDoc.getElementsByTagName("CD");
+    var str = ("<table class=\"table table-condensed\">" +
+        "<thead><tr><td>TITLE</td><td>ARTIST</td><td>COUNTRY</td>" +
+        "<td>COMPANY</td><td>PRICE</td><td>YEAR</td></tr></thead><tbody>");
+    for (var i = 0; i < allCdNodes.length; i++) {
+        str += ("<tr><td>");
+        str += (allCdNodes[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue);
+        str += ("</td><td>");
+        str += (allCdNodes[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue);
+        str += ("</td><td>");
+        str += (allCdNodes[i].getElementsByTagName("COUNTRY")[0].childNodes[0].nodeValue);
+        str += ("</td><td>");
+        str += (allCdNodes[i].getElementsByTagName("COMPANY")[0].childNodes[0].nodeValue);
+        str += ("</td><td>");
+        str += (allCdNodes[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue);
+        str += ("</td><td>");
+        str += (allCdNodes[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue);
+        str += ("</td></tr>");
+    }
+    str += "</tbody></table>";
+    $("#tableCdDiv").html(str);
 }
 
 function updatePaging() {
@@ -137,5 +142,5 @@ function updatePaging() {
         $("#first").attr("disabled", true);
         $("#last").attr("disabled", true);
     }
-    $("#secondDiv tbody").html("");
+    $("#secondDiv").find("tbody").html("");
 }
